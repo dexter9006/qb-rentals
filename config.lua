680023c767a1f9f149618bcc5ca66baba6c35e70
dexter9006 <52006237+dexter9006@users.noreply.github.com>
2024-02-03 03:18:40 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -5,38 +5,38 @@ Config.VehicleLocation = vector4(-1018.98, -2701.22, 13.76, 60.52)
 
 Config.Vehicles= {
         {
-                title = "Euros",
-                name = "euros",
-                image = "https://cdn.discordapp.com/attachments/905103108382801961/1195858390522548284/image.png?ex=65b584ea&is=65a30fea&hm=cc930e0167d90347ff2e9a73e9bea3323bf0823ea4414776bf67a73232bc79de&",
-                price = 150,
-        },
-        {
-                title = "Vectre",
-                name = "vectre",
-                image = "https://cdn.discordapp.com/attachments/905103108382801961/1195858829808783491/image.png?ex=65b58553&is=65a31053&hm=933370240a7853cea8d17a97115cab46631a9fa3fc59e21904acdb2c632250da&",
+                title = "Asbo",
+                name = "asbo",
+                image = "https://static.wikia.nocookie.net/gtawiki/images/b/bd/Asbo-GTAO-front.png",
                 price = 125,
         },
         {
-                title = "Rhinehart",
-                name = "rhinehart",
-                image = "https://cdn.discordapp.com/attachments/905103108382801961/1195859259162890280/image.png?ex=65b585b9&is=65a310b9&hm=f3e648e5e90afc4431dced39794db889a1850a72ceb46b4b79ab1c600a9d34f9&",
-                price = 100,
+                title = "Blista",
+                name = "blista",
+                image = "https://static.wikia.nocookie.net/gtawiki/images/c/c0/Blista-GTAV-front.png",
+                price = 150,
         },
         {
-                title = "Champion",
-                name = "champion",
-                image = "https://cdn.discordapp.com/attachments/905103108382801961/1195859817886134372/image.png?ex=65b5863e&is=65a3113e&hm=bfcff4285e5cfd40f107392ba085c10e218647e9fe062b248f22d54e8209a817&",
-                price = 115,
+                title = "Rhapsody",
+                name = "rhapsody",
+                image = "https://static.wikia.nocookie.net/gtawiki/images/c/cc/Rhapsody-GTAV-front.png",
+                price = 175,
+        },
+        {
+                title = "Panto",
+                name = "panto",
+                image = "https://static.wikia.nocookie.net/gtawiki/images/a/ad/Panto-GTAV-front.png",
+                price = 100,
         },
 }
 
 Config.Notify = {
-        timeout = "The Rental Got Taken From You", "success",
-        returned = "You Returned The Vehicle", "success",
-        nomoney = "You Dont Have Enough Money", "error",
-        tofar = "You Are To Far Away From The Rental Vehicle", "error",
-        wrongvehicle = "This Is Not The Vehicle You Rented", "error",
-        notrenting = "You Are Not Renting A Vehicle Yet", "error",
-        alreadyrenting = "You Are Already Renting A Vehicle", "error",
-        rented = "You Rented A Vehicle", "success",
+        timeout = "La location vous a été retirée", "succès",
+        returned = "Vous avez rendu le véhicule", "succès",
+        nomoney = "Vous n'avez pas assez d'argent", "erreur",
+        toofar = "Vous êtes trop loin du véhicule de location", "erreur",
+        wrongvehicle = "Ce n'est pas le véhicule que vous avez loué", "erreur",
+        notrenting = "Vous ne louez pas encore de véhicule", "erreur",
+        alreadyrenting = "Vous louez déjà un véhicule", "erreur",
+        rented = "Vous avez loué un véhicule", "succès",
 }
